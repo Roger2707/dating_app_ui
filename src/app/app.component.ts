a012b2dfaefe53854c9a92fc9de75220e9bd2695
Roger <rogerhuynh2707@gmail.com>
2025-06-12 15:08:42 +0700

    First commit 12/06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'dating_app_ui';
+  users: any;
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => this.users = response,
+      error: (error: any) => console.log(error),
+      complete: () => console.log('request completed !')
+    })
+  }
 }
